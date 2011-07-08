6dee74af25d7c77c144f1af5d35ea6f85aa0abad
Justin M. LaPre <justin@lapre.com>
2011-07-08 14:10:14 -0400

    And copied it over here.

--- Foo4/run.sh
+++ Foo4/run.sh
@@ -1,12 +1,46 @@
 #!/bin/sh
 
-export PATH=/Users/jlapre/temp/llvm-mac-install/bin:/Users/jlapre/temp/llvm-gcc/bin:$PATH
+# Army directory layout
+DIRECTORY="/Users/jlapre/temp/llvm-mac-install/bin"
+
+if [ -d "$DIRECTORY" ]; then
+#    echo "$DIRECTORY found"
+    export PATH="$DIRECTORY":$PATH
+    LIB="/Users/jlapre/temp/llvm-mac-install/lib/LLVMFoo3.dylib"
+#    echo $PATH
+fi
+
+DIRECTORY="/Users/jlapre/temp/llvm-gcc/bin"
+
+if [ -d "$DIRECTORY" ]; then
+#    echo "$DIRECTORY found"
+    export PATH="$DIRECTORY":$PATH
+#    echo $PATH
+fi
+
+# odin directory layout
+DIRECTORY="/Users/laprej/temp/llvm-mac-install/bin"
+
+if [ -d "$DIRECTORY" ]; then
+#    echo "$DIRECTORY found"
+    export PATH="$DIRECTORY":$PATH
+    LIB="/Users/laprej/temp/llvm-mac-install/lib/LLVMFoo3.dylib"
+#    echo $PATH
+fi
+
+DIRECTORY="/Users/laprej/temp/llvm-gcc/bin"
+
+if [ -d "$DIRECTORY" ]; then
+#    echo "$DIRECTORY found"
+    export PATH="$DIRECTORY":$PATH
+#    echo $PATH
+fi
 
 llvm-gcc -emit-llvm hello.c -c -o hello.bc
 
 llvm-gcc -emit-llvm hello.c -S -o hello.ll
 
-opt -debug -load /Users/jlapre/temp/llvm-mac-install/lib/LLVMFoo4.dylib -hello -rev-func=foobar -tgt-func=barbar < hello.bc > test.bc
+opt -debug -load "$LIB" -hello -rev-func=foobar -tgt-func=barbar < hello.bc > test.bc
 
 llc -march=c test.bc
 
