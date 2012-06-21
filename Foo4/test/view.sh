9715275eaf844a6e7a51d9ac008d7db48533eed1
Justin M. LaPre <justin@lapre.com>
2012-06-21 16:34:23 -0400

    Altered paths in view.sh for army machine.

--- Foo4/test/view.sh
+++ Foo4/test/view.sh
@@ -12,6 +12,13 @@ then
     exit
 fi
 
-export PATH=/Users/laprej/temp/llvm-3.0-install/bin:$PATH
+#odin
+if [ -d /Users/laprej/temp/llvm-3.0-install/bin ]; then
+    export PATH=/Users/laprej/temp/llvm-3.0-install/bin:$PATH
+fi
+#army
+if [ -d /Users/jlapre/temp/llvm-mac-install/bin ]; then
+    export PATH=/Users/jlapre/temp/llvm-mac-install/bin:$PATH
+fi
 
 llvm-as < $1 | opt -analyze -view-cfg
