e0ec5fc1e394c694e641fd7a308b1f443d7b2234
Justin M. LaPre <justin@lapre.com>
2013-10-31 14:04:14 -0400

    Removed some scratch-work code that was killing me.

--- Foo4/Hello.cpp
+++ Foo4/Hello.cpp
@@ -559,59 +559,6 @@ namespace {
                 DEBUG(errs() << "Storing to non-LP state variable\n");
                 Value *v = cast<Value>(&I);
                 markJML(v);
-                
-                Function *f = M.getFunction(FuncToInstrument);
-                std::vector<Argument *> funArgsFrom;
-                for (Function::arg_iterator iter = f->arg_begin(),
-                     end = f->arg_end(); iter != end; ++iter) {
-                    funArgsFrom.push_back(iter);
-                }
-                                
-                Type *Ty = 0;
-                
-                FunctionType *funTypeFrom = f->getFunctionType();
-                
-                std::vector<Type *> funTypeArgsFrom(funTypeFrom->param_begin(), funTypeFrom->param_end());
-                Ty = funTypeArgsFrom[2];
-                
-                assert(Ty);
-                assert(isa<PointerType>(Ty));
-                
-                Ty = Ty->getPointerElementType();
-                
-                assert(isa<StructType>(Ty));
-                
-                StructType *fromStruct = cast<StructType>(Ty);
-                
-                std::vector<Type *> structItems(fromStruct->element_begin(), fromStruct->element_end());
-                
-                StructType *toStruct = StructType::create(structItems, fromStruct->getName(), fromStruct->isPacked());
-                
-                PointerType *toStructPtr = PointerType::getUnqual(toStruct);
-                
-                Argument *newArg = new Argument(toStructPtr, funArgsFrom[2]->getName(), 0);
-                
-                DEBUG(errs() << "Created new Argument " << newArg->getName() << "\n");
-                
-                std::vector<Type *> funTypeArgsTo(funTypeArgsFrom.begin(), funTypeArgsFrom.end());
-                funTypeArgsTo[2] = toStructPtr;
-                
-                std::vector<Argument *> funArgsTo(funArgsFrom.begin(), funArgsFrom.end());
-                funArgsTo[2] = newArg;
-                
-                FunctionType *funTypeTo = FunctionType::get(f->getReturnType(), funTypeArgsTo, false);
-                
-                Function *newFun = Function::Create(funTypeTo, Function::ExternalLinkage, f->getName());
-                
-                ValueToValueMapTy vmap;
-                for (unsigned i = 0; i < funArgsFrom.size(); ++i) {
-                    vmap.insert(std::make_pair(funArgsFrom[i], funArgsTo[i]));
-                }
-                
-                SmallVector<ReturnInst*, 4> Returns;
-                
-                CloneFunctionInto(newFun, f, vmap, false, Returns);
-
                 return;
             }
 
